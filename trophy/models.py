01c9e2f819475e2e27f30d9d1b95302996046b75
yontey <muasajohn01@gmail.com>
2019-05-25 18:13:05 +0300

    added Projects model

--- trophy/models.py
+++ trophy/models.py
@@ -2,5 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=20,blank=True)
+    image_landing = models.ImageField(upload_to='landing/')
+    description = models.TextField(max_length=200,blank=True)
+    link = models.CharField(max_length=20)
+    
+    
+
 
 
