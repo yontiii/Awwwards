2916230db4f6030e0605fd4ce7baf2a545fb94cf
yontey <muasajohn01@gmail.com>
2019-05-25 16:22:10 +0300

    Started project and initialized the app

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/moringa-school-projects/Awards/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
