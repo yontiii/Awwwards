f65f2a0d90d8017d1f909e7f0302ae2f438c9a6d
yontey <muasajohn01@gmail.com>
2019-05-26 16:29:31 +0300

    Configured media configurations to handle photos and static files

--- trophy/models.py
+++ trophy/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 from url_or_relative_url_field.fields import URLOrRelativeURLField
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
     profile_photo = models.ImageField(default='default.jpg',upload_to='profiles/')
-    bio = models.TextField(max_length=500,default='Tell Me Something')
-    website = models.CharField(max_length=10, blank=True,default='me.com') 
+    bio = HTMLField(max_length=500,default='Tell Me Something')
+    website = URLOrRelativeURLField() 
     phone_number = models.CharField(max_length=10,default=12345678)
     
     
@@ -29,12 +30,11 @@ class Profile(models.Model):
     
     
 class Projects(models.Model):
-    user = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
-    profile = models.ForeignKey(User,on_delete=models.CASCADE) 
+    user = models.ForeignKey(User,null=True,on_delete=models.CASCADE) 
     title = models.CharField(max_length=20,blank=True)
     image_landing = models.ImageField(upload_to='landing/')
-    description = models.TextField(max_length=200,blank=True)
-    link = models.URLOrRelativeURLField(max_length=200)
+    description = HTMLField(max_length=200,blank=True)
+    link = URLOrRelativeURLField(max_length=200)
     
     
     @classmethod
