7863177fce0fb7e657cdc83af243234fd061066a
yontey <muasajohn01@gmail.com>
2019-05-26 23:30:53 +0300

    Created homepage to display all projects done

--- trophy/views.py
+++ trophy/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Profile,Projects
 # Create your views here.
 def home(request):
+    projects = Projects.objects.all()
     
-    return render(request,'home.html')
+    context = {
+        'projects':projects,
+    }
+    return render(request,'home.html',context)
