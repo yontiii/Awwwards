38a38797765cc6d23201192751cf68f3597561d2
yontey <muasajohn01@gmail.com>
2019-05-29 19:11:26 +0300

    Staged changes for deployment

--- trophy/models.py
+++ trophy/models.py
@@ -7,28 +7,6 @@ from tinymce.models import HTMLField
 from url_or_relative_url_field.fields import URLOrRelativeURLField
 
 # Create your models here.
-
-class Projects(models.Model): 
-    user = models.ForeignKey(User,null=True,on_delete=models.CASCADE) 
-    title = models.CharField(max_length=20,blank=True)
-    design=models.IntegerField(default=0)
-    usability=models.IntegerField(default=0)
-    content=models.IntegerField(default=0)
-    image_landing = models.ImageField(upload_to='landing/')
-    description = HTMLField(max_length=200,blank=True)
-    link = URLOrRelativeURLField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    
-    
-    @classmethod
-    def search_by_projects(cls,search_term):
-        projects = cls.objects.filter(title__icontains=search_term)
-        print(projects)
-        return projects 
-    
-    
-    def __str__(self):
-        return self.title
     
     
 class Profile(models.Model):
@@ -37,7 +15,7 @@ class Profile(models.Model):
     bio = HTMLField(max_length=500,default='Tell Me Something')
     website = URLOrRelativeURLField() 
     phone_number = models.CharField(max_length=10,default=12345678)
-    projects = models.ForeignKey(Projects,on_delete=models.CASCADE,null=True)
+    # projects = models.ForeignKey(Projects,on_delete=models.CASCADE,null=True)
     
     @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
@@ -63,6 +41,35 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.bio
+    
+    
+class Projects(models.Model): 
+    profile = models.ForeignKey(User,null=True,on_delete=models.CASCADE) 
+    title = models.CharField(max_length=20,blank=True)
+    design=models.IntegerField(default=0)
+    usability=models.IntegerField(default=0)
+    content=models.IntegerField(default=0)
+    image_landing = models.ImageField(upload_to='landing/')
+    description = HTMLField(max_length=200,blank=True)
+    link = URLOrRelativeURLField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    
+    @classmethod
+    def search_by_projects(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        print(projects)
+        return projects 
+    
+    @classmethod
+    def get_profile_projects(cls,profile):
+       projects = Projects.objects.filter(profile__pk=profile)
+       print(projects)
+       return projects
+    
+    
+    def __str__(self):
+        return self.title
     
 
 class Rates(models.Model):
--- trophy/serializer.py
+++ trophy/serializer.py
@@ -4,7 +4,7 @@ from .models import Profile,Projects
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('user','profile_photo','bio','projects') 
+        fields = ('user','profile_photo','bio') 
         
         
 class ProjectsSerializer(serializers.ModelSerializer):
