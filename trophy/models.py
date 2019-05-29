051e1c6ccceac9ffd1a398032ae8ccb0ab76fdfc
yontey <muasajohn01@gmail.com>
2019-05-29 08:44:20 +0300

    Created templates for the forms

--- trophy/models.py
+++ trophy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
+from django.core.validators import MaxValueValidator
 from django.dispatch import receiver
 from django.contrib.auth.models import User 
 from tinymce.models import HTMLField
